0a387bbb19e4b3d3a402ad8080badbc1899eb708
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-04 10:33:53 +0300

    feat: update the result

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ module.exports = function reverse(n) {
 	let strN = String(n);
 	for (let i = 0; i < strN.length; i++) {
 		result = `${strN[i]}${result}`;
+	} 
+	if (strN[0] == '-') {
+		return parseFloat(result) * -1;
+	} else {
+		return parseFloat(result);
 	}
-	return parseFloat(result) * -1;
 }
