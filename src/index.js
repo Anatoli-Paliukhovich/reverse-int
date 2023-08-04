cc5fad941cf58509a83bb11a3a031460dee3dd2c
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-04 10:38:22 +0300

    fix: update the task

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ module.exports = function reverse(n) {
 	for (let i = 0; i < strN.length; i++) {
 		result = `${strN[i]}${result}`;
 	} 
-	if (strN[0] == '-') {
-		return parseFloat(result) * -1;
-	} else {
-		return parseFloat(result);
-	}
+	return parseFloat(result) * Math.sign(n);
 }
