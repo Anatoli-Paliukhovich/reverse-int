d5e2532ed30903816006c288c8c1b79cbcb940e5
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-04 10:28:48 +0300

    docs: resolve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let result = '';
+	let strN = String(n);
+	for (let i = 0; i < strN.length; i++) {
+		result = `${strN[i]}${result}`;
+	}
+	return parseFloat(result) * -1;
 }
